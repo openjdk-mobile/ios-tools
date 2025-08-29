03167f3d15a13175675d7dcc54127eb113d6c076
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2025-08-29 09:03:46 +0530

    rename variables

--- .github/scripts/build-xcframework.sh
+++ .github/scripts/build-xcframework.sh
@@ -4,16 +4,16 @@ set -euxo pipefail
 # Define constants
 LIBFFI=./libffi-ios
 LIBFFI_SIM=./libffi-ios-sim
-DEVICE_SRC=./device
-SIMULATOR_SRC=./simulator
+OPENJDK_DEVICE_BUILD=./device
+OPENJDK_SIMULATOR_BUILD=./simulator
 DEVICE_TARGET=./device-static
 SIMULATOR_TARGET=./sim-static
 
 # Create device static
 mkdir $DEVICE_TARGET
 cp $LIBFFI/libffi.a $DEVICE_TARGET
-cp $DEVICE_SRC/images/static-libs/lib/*.a $DEVICE_TARGET
-cp $DEVICE_SRC/images/static-libs/lib/zero/libjvm.a $DEVICE_TARGET
+cp $OPENJDK_DEVICE_BUILD/images/static-libs/lib/*.a $DEVICE_TARGET
+cp $OPENJDK_DEVICE_BUILD/images/static-libs/lib/zero/libjvm.a $DEVICE_TARGET
 cd $DEVICE_TARGET
 libtool -static -o libdevice.a libjvm.a libffi.a libjava.a libzip.a libnet.a libnio.a libjimage.a
 cd ..
@@ -21,20 +21,20 @@ cd ..
 # Create sim static
 mkdir $SIMULATOR_TARGET
 cp $LIBFFI_SIM/libffi.a $SIMULATOR_TARGET
-cp $SIMULATOR_SRC/images/static-libs/lib/*.a $SIMULATOR_TARGET
-cp $SIMULATOR_SRC/images/static-libs/lib/zero/libjvm.a $SIMULATOR_TARGET
+cp $OPENJDK_SIMULATOR_BUILD/images/static-libs/lib/*.a $SIMULATOR_TARGET
+cp $OPENJDK_SIMULATOR_BUILD/images/static-libs/lib/zero/libjvm.a $SIMULATOR_TARGET
 cd $SIMULATOR_TARGET
 libtool -static -o libdevice.a libjvm.a libffi.a libjava.a libzip.a libnet.a libnio.a libjimage.a
 cd ..
 
 # Flatten header location
-cp $DEVICE_SRC/jdk/include/ios/* $DEVICE_SRC/jdk/include/
-cp $SIMULATOR_SRC/jdk/include/ios/* $SIMULATOR_SRC/jdk/include/
+cp $OPENJDK_DEVICE_BUILD/jdk/include/ios/* $OPENJDK_DEVICE_BUILD/jdk/include/
+cp $OPENJDK_SIMULATOR_BUILD/jdk/include/ios/* $OPENJDK_SIMULATOR_BUILD/jdk/include/
 
 # Create XCFramework
 xcodebuild -create-xcframework \
   -library $DEVICE_TARGET/libdevice.a \
-  -headers $DEVICE_SRC/jdk/include \
+  -headers $OPENJDK_DEVICE_BUILD/jdk/include \
   -library $SIMULATOR_TARGET/libsim.a \
-  -headers $SIMULATOR_SRC/jdk/include \
+  -headers $OPENJDK_SIMULATOR_BUILD/jdk/include \
   -output ./OpenJDK.xcframework

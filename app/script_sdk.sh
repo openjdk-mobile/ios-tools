020c765b327b5119503dfc56d5495004896b15cd
jose.pereda <jose.pereda@gluonhq.com>
2026-02-08 19:47:56 +0100

    remove upstreamed patch

--- app/script_sdk.sh
+++ app/script_sdk.sh
@@ -50,7 +50,6 @@ if [[ ! -d "jfx" ]]; then
   git clone --depth 1 https://github.com/openjdk/jfx.git
   cd jfx || exit
   git apply "$root/../../.github/patches/jfx-es2-patch.diff"
-  git apply "$root/../../.github/patches/jfx-pthread-patch.diff"
   git apply "$root/../../.github/patches/jfx-glass-patch.diff"
   sh gradlew shadersClasses
   cd ..
